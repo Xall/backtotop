68140af969d910a7b8cfca845445ee1b238f5cff
Xall <simon.volpert@googlemail.com>
2013-02-19 18:42:29 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+
+This is a small Mootools snippet, to create a little fixed button on the bottom of the page, to scroll back to top.
+
+# Usage
+There are no changes in your HTML code nescessary. Just import MooTools and this Script+CSS. To customize the apperance of the button, change the the CSS of `#backtotop` and `#backtotop:hover`.
---
+++ backtotop.js
@@ -0,0 +1,22 @@
+window.addEvent('domready', function() {
+    var body = $$('body')[0];
+    var container = new Element('a#backtotop',{
+        html: '^<br>Back to top',
+        href: '#',
+    });
+    container.inject(body);
+    var myFx = new Fx();
+
+    container.addEvent('click', function () {
+        container.fade('out');
+    });
+
+    window.addEvent('scroll', function(){
+        if (window.pageYOffset>0)
+            container.fade('in');
+        else
+            container.fade('out');
+    });
+
+
+});
